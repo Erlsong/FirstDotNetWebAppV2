0884bed62eb8de1e70e7ba2ac7b3e4bfd125eef2
Erlsong <bowejessop@gmail.com>
2025-08-04 22:43:29 -0600

    swagger installed and injected

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,5 +1,6 @@
-using Microsoft.EntityFrameworkCore;
 using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,17 +11,33 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(); 
+}
+app.MapGet("/", () => Results.Redirect("/swagger"));
+
+
 // Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
+app.MapControllers(); 
 
-app.MapControllers();
+
+
 
 app.Run();
